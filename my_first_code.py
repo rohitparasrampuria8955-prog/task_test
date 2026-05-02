332adf3d10723853f5089d73e6b9980c7720df92
Rohit <rohitparasrampuria8955@gmail.com>
2026-05-02 10:41:34 +0530

    Revert "new commit added"
    This reverts commit 2e123a11c1db0391caa7cb28e4c9a053c1573f92.

--- my_first_code.py
+++ my_first_code.py
@@ -5,3 +5,5 @@ c=30
 sum=0
 sum=a+b+c
 print(sum)
+# by mistake rong commit push kar diya to usko reverback kaise karenge
+# by mistake git add and coomit kar diya is ko privios area ya stagin area se kaise remove kare
